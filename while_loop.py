d328ea5d2b9dd09b7d069fae1377176ffbd09456
David Peralez <davidperalez89@gmail.com>
2024-08-04 14:53:12 -0700

    updated while loops

--- while_loop.py
+++ while_loop.py
@@ -3,24 +3,24 @@
 # The loop only breaks/ends once the word 'done' is entered
 # Everytime the loops runs, print "We got your message"
 
-# message = input("Enter a message (type 'done' to end): ").lower()
+message = input("Enter a message (type 'done' to end): ").lower()
 
-# while message != 'done':
-#     print("We got your message!")
-#     message = input("Enter a message (type 'done' to end): ").lower()
-# else:
-#     print("Thank you")
+while message != 'done':
+    print("We got your message!")
+    message = input("Enter a message (type 'done' to end): ").lower()
+else:
+    print("Thank you")
 
 #---------- Challenge 2 ----------
 # Create an account login
 # The user has 2 passwords, either one can be entered to gain access
 # Once the user enters the correct password, greet them with "Welcome to your account"
 
-# password = input("Enter your password: ")
+password = input("Enter your password: ")
 
-# while password != "tars" and password != "case":
-#     password = input("INVALID PASSWORD. TRY AGAIN: ")
-# print("Welcome to your account")
+while password != "tars" and password != "case":
+    password = input("INVALID PASSWORD. TRY AGAIN: ")
+print("Welcome to your account")
 
 
 #---------- Challenge 3 ----------
@@ -28,28 +28,28 @@
 # For every person, print "Congrats [name]. You saved 20%"
 # Once 3 people have gotten the discount, print "All done!"
 
-# counter = 0
+counter = 0
 
-# while counter < 3:
-#     name = input("Enter your name: ")
-#     print(f"Congrats {name}, you saved 20%!")
-#     counter += 1
-# print("All done!")
+while counter < 3:
+    name = input("Enter your name: ")
+    print(f"Congrats {name}, you saved 20%!")
+    counter += 1
+print("All done!")
 
 
 #---------- Challenge 4 ----------
 # A user must enter a programming language, but the only get 5 tries
 # If the user enters Python, print off how many tries it took them
 
-# tries = 0
-# code = ""
+tries = 0
+code = ""
 
-# while tries < 5 and code != 'python':
-#     code = input("Enter a programming language: ")
-#     tries += 1
+while tries < 5 and code != 'python':
+    code = input("Enter a programming language: ")
+    tries += 1
 
-# if code == 'python':
-#     print(f"It took you {tries} tries")
+if code == 'python':
+    print(f"It took you {tries} tries")
 
 
 
@@ -58,14 +58,14 @@
 # The program begins when the user enters 0 and ends when the user enters 1
 # For every new train ticket, print off the train ticket number
 
-# tickets = input("Enter 1 - book or 2 - end booking: ")
-# i = 1
+tickets = input("Enter 1 - book or 2 - end booking: ")
+i = 1
 
-# while tickets != '2':
-#     if tickets == '1':
-#         print(f"train ticket:{i}")
-#         i += 1
-#     tickets = input("Enter 1 - book or 2 - end booking: ")
+while tickets != '2':
+    if tickets == '1':
+        print(f"train ticket:{i}")
+        i += 1
+    tickets = input("Enter 1 - book or 2 - end booking: ")
 
 
 
