24a047894b3a18d2f301ca245417b0edcd29b211
David Peralez <davidperalez89@gmail.com>
2024-08-06 09:38:50 -0700

    starting project to inlcude everything learned so far

---
+++ madferitGPT/madferitGPT.py
@@ -0,0 +1,15 @@
+# Suggest Oasis albums based on mood or fan status
+# Once album is suggested, offer 1-2 song recommendations
+#  If they like the music and want to know more suggest Documentary. Oasis: Supersonic 2021
+
+# Input statement ot start the system
+# Main While Loop (Repeat until an event)
+# Conditional Statements (if/elif/else)
+# print()
+# Try adding the for loop
+# 5 or more main actions
+# Nesting is key!
+
+
+print("Welcome to Madferit 2.0. Lets get you listening to Oasis!")
+enter = int(input(" 1 - Start. 2 - Quit: "))
