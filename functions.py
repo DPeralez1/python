67dc0a079615bfaf27c23e36fd737047853418e9
David Peralez <davidperalez89@gmail.com>
2024-08-07 19:07:38 -0700

    added two bonus challenges

--- functions.py
+++ functions.py
@@ -6,25 +6,20 @@
 # otherwise print, "You are in the top of the class"
 # use an input to collect the score then call the function
 
-# def grade(score):
-#     print("Your score is: ")
-#     if score > 0 and score <= 50:
-#         print("Below passing, Improve!")
-#     elif score > 50 and score <=70:
-#         print("Barley passing")
-#     elif score > 70 and score <= 90:
-#         print("You're passing!")
-#     else:
-#         print("You're top of the class!")
+def grade(score):
+    print("Your score is: ")
+    if score > 0 and score <= 50:
+        print("Below passing, Improve!")
+    elif score > 50 and score <=70:
+        print("Barley passing")
+    elif score > 70 and score <= 90:
+        print("You're passing!")
+    else:
+        print("You're top of the class!")
 
 
-# score = int(input("Enter your score: "))
-# grade(score)
-
-
-
-
-
+score = int(input("Enter your score: "))
+grade(score)
 
 
 #---------- Challenge 2 ----------
@@ -34,21 +29,21 @@
 # before returing the price you need to add tax of 0.08%, then return the price
 # print off the grand total after extras and tax
 
-# def flight_cost(base_fare):
-#     upgrade = input("Do you want to upgrade: (y/n)").lower()
-#     if upgrade == 'y':
-#         base_fare += 99
-#     baggage = input("Will you have bags? (y/n)").lower()
-#     if baggage == 'y':
-#        base_fare += 35
+def flight_cost(base_fare):
+    upgrade = input("Do you want to upgrade: (y/n)").lower()
+    if upgrade == 'y':
+        base_fare += 99
+    baggage = input("Will you have bags? (y/n)").lower()
+    if baggage == 'y':
+       base_fare += 35
 
-#     tax = (base_fare * 0.08) + base_fare
-#     return tax
+    tax = (base_fare * 0.08) + base_fare
+    return tax
 
 
-# base_fare = int(input("Enter the base fare:"))
-# grand_total = flight_cost(base_fare)
-# print(f"Grand total after tax: {grand_total:.2f}")
+base_fare = int(input("Enter the base fare:"))
+grand_total = flight_cost(base_fare)
+print(f"Grand total after tax: {grand_total:.2f}")
 
 
 
@@ -61,23 +56,23 @@
 # if the function returns True, print off "no need to worry" else print "Your account is low"
 
 
-# def total(balance):
-#     if balance >= 500:
-#         return True
-#     else:
-#         return False
+def total(balance):
+    if balance >= 500:
+        return True
+    else:
+        return False
 
-# customers = int(input("Number of customers? "))
-# for i in range(customers):
-#     name = input("What is your first name? ")
-#     balance = float(input("What is your current balance? "))
-#     res = total(balance)
+customers = int(input("Number of customers? "))
+for i in range(customers):
+    name = input("What is your first name? ")
+    balance = float(input("What is your current balance? "))
+    res = total(balance)
 
-#     print(f"Enough funds in {name}'s account: ", res)
-#     if res == True:
-#         print("Don't worry you have enough funds")
-#     else:
-#         print("Your account is getting low!")
+    print(f"Enough funds in {name}'s account: ", res)
+    if res == True:
+        print("Don't worry you have enough funds")
+    else:
+        print("Your account is getting low!")
 
 
 
@@ -103,3 +98,64 @@ while saved != 0:
     mortgage(saved)
     saved = int(input("Deposit amount: "))
 print(f"Total money requested: {total_balance}")
+
+#---------- Bonus Challange ----------
+# create 4 functions, the first 3 will be terminal_1, terminal_2, terminal_3
+# When one of those functions are called, print off which terminal to go to the call the main function again
+# the main function should ask "is your flight budget/domestic or international" when called
+# budget - terminal 1 , domestic - terminal 2, international - terminal 3
+
+def terminal_1():
+    print("Departing from terminal 1 - Budget")
+    flight_check()
+
+def terminal_2():
+    print("Departing from terminal 2 - Domestic")
+    flight_check()
+
+def terminal_3():
+    print("Departing from terminal 3 - International")
+    flight_check()
+
+
+def flight_check():
+    answer = input("Budget/Domestic or International: ").lower()
+    if answer == "budget":
+        terminal_1()
+    elif answer == "domestic":
+        terminal_2()
+    elif answer == "international":
+        terminal_3()
+    else:
+        print("There is no other terminal!")
+
+flight_check()
+
+
+
+#---------- Bonus Challange  ----------
+# POPULAR - Create a BMI calculator using 2 functions
+# one function calculates and returns the BMI (BMI = weight / (height * height))
+# the second function takes the bmi results and tells you if you are underweight, normal weight or overweight
+# Create a loop to run for a number of people
+# pass the weight and height into one of the functions which will call the first function
+
+
+def calculate_bmi(weight, height):
+    calculation = weight / (height * height)
+    return calculation
+
+def show_results(weight, height):
+    calculate = calculate_bmi(weight, height)
+    if calculate <= 18.5:
+        print("Underweight")
+    elif calculate > 18.5 and calculate <= 25:
+        print("Normal")
+    else:
+        print("Overweight")
+
+people = int(input("Enter number of people: "))
+for i in range(people):
+    weight = float(input("Enter weight: "))
+    height = float(input("Enter height: "))
+    show_results(weight, height)
