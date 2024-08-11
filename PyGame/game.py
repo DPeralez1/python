66120d2d6d402b5f757d34c749de978ce239be22
David Peralez <davidperalez89@gmail.com>
2024-08-10 17:05:13 -0700

    updated images

--- PyGame/game.py
+++ PyGame/game.py
@@ -10,3 +10,19 @@ speed = 10
 screen = display.set_mode((WIDTH, HEIGHT))
 display.set_caption("Fly Me")
 clock = time.Clock()
+background = transform.scale(image.load("desert.png"), (WIDTH, HEIGHT))
+airplane = transform.scale(image.load("airplane.png"), (200,100))
+
+
+
+
+
+run = True
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            quit()
+            run = False
+
+    display.update()
+    clock.tick(FPS)
