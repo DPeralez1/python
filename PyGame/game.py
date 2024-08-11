830695a314a1790725b06ef2bf362aa22e531534
David Peralez <davidperalez89@gmail.com>
2024-08-10 17:14:39 -0700

    Intro into Pygame and created demo game

--- PyGame/game.py
+++ PyGame/game.py
@@ -10,8 +10,8 @@ speed = 10
 screen = display.set_mode((WIDTH, HEIGHT))
 display.set_caption("Fly Me")
 clock = time.Clock()
-background = transform.scale(image.load("desert.png"), (WIDTH, HEIGHT))
-airplane = transform.scale(image.load("airplane.png"), (200,100))
+background = transform.scale(image.load("./Pygame/desert.png"), (WIDTH, HEIGHT))
+airplane = transform.scale(image.load("./Pygame/airplane.png"), (200,100))
 
 
 
@@ -19,10 +19,26 @@ airplane = transform.scale(image.load("airplane.png"), (200,100))
 
 run = True
 while run:
+    screen.blit(background,(0,0))
+    screen.blit(airplane,(plane_x, plane_y))
     for e in event.get():
         if e.type == QUIT:
             quit()
             run = False
+
+
+    keys = key.get_pressed()
+    if keys[K_LEFT] and plane_x > 5:
+        plane_x -= speed
+    if keys[K_RIGHT] and plane_x < WIDTH - 200:
+        plane_x += speed
+
+    if keys[K_UP] and plane_y > 5:
+        plane_y -= speed
+    if keys[K_DOWN] and plane_y < HEIGHT - 100:
+        plane_y += speed
+
+
 
     display.update()
     clock.tick(FPS)
